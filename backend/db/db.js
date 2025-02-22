b477a58f4af7e3fc840d987798f5fc548a79ace0
ujjwal738 <112887468+ujjwal738@users.noreply.github.com>
2025-02-23 00:32:22 +0530

    hehe

---
+++ backend/db/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+function connectToDb(){
+    mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('connected to DB'))
+        .catch(err => console.log(err));
+}
+
+module.exports = connectToDb;
