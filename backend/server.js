934f94070ad58c99fcccd02e5fb63f5a6ef99d4e
ujjwal738 <112887468+ujjwal738@users.noreply.github.com>
2025-02-23 00:06:29 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv')
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const cors = require('cors');
+
+const http = require('http');
+
+const app = require('./app');
+const { log } = require('console');
+
+const server = http.createServer(app);
+
+server.listen(port, ()=>{
+    console.log(`server is running on port, ${port}`)
+});
